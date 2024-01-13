8dc0ee494f04b7de2935519cd38e44aadb59e453
isama78 <isa21005@byui.edu>
2024-01-13 19:22:26 -0300

    resume task done

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,11 @@
+public class Job 
+{
+    public string _company = "";
+    public string _jobTitle = "";
+    public string _startYear = "";
+    public string _endYear = "";
+
+    public void DisplayJobInformation () {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = "2018";
+        job1._endYear = "2020";
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = "2011";
+        job2._endYear = "2023";
+
+        // job1.DisplayJobInformation();
+        // job2.DisplayJobInformation();
+
+        Resume resume = new Resume();
+        resume._name = "Mauro Isa";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        // Console.WriteLine(resume._jobs[0]._jobTitle);
+        resume.DisplayDetails();
     }
 }
