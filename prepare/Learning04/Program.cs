38b4fbc450c9664954d78375493390367e90e7fb
isama78 <isa21005@byui.edu>
2024-01-30 10:21:12 -0300

    Inheritance Learning Activity done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment newAssingment = new Assignment("Mauro Isa", "Programming");
+        Console.WriteLine(newAssingment.GetSummary());
+        MathAssignment newMathAssignment = new MathAssignment("Mauro Isa", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(newMathAssignment.GetHomeworkList());
+        WritingAssignment newWritingAssignment = new WritingAssignment("Mauro Isa", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(newWritingAssignment.GetWritingInformation());
     }
 }
